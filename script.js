cdd0bd4d08b0f68c7b1a885238a37b79fd61fa23
rchiarelli <renan_chiarelli@hotmail.com>
2026-01-17 19:16:22 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-});
-
-let quadrados = document.querySelectorAll(".quadrados")
-
-for (let quadrado of quadrados) {
-    quadrado.addEventListener("click", function () {
-        if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "O") {
-            quadrado.firstElementChild.textContent = "X"
-        } else if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "X") {
+    
+    let quadrados = document.querySelectorAll(".quadrados")
+    
+    for (let quadrado of quadrados) {
+        quadrado.addEventListener("click", function() {
+            if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "O") {
+                quadrado.firstElementChild.textContent = "X"
+            } else if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "X") {
                 quadrado.firstElementChild.textContent = "O"
-        }
-    });
-}
+            }
+        });
+    }
+});
