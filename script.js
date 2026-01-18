6ce8c8f686517cc4429d4b77a3cb0d0ccb21e589
rchiarelli <renan_chiarelli@hotmail.com>
2026-01-18 14:14:35 -0300

    atualização css, js pra linha de vitória

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     let quadrados = document.querySelectorAll(".quadrados");
+    let jogadorAtual = "X"
     
     for (let quadrado of quadrados) {
         quadrado.addEventListener("click", function() {
-            if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "O") {
-                quadrado.firstElementChild.textContent = "X"
-            } else if (quadrado.firstElementChild.textContent === "" || quadrado.firstElementChild.textContent === "X") {
-                quadrado.firstElementChild.textContent = "O"
+            if (quadrado.firstElementChild.textContent === "") {
+                quadrado.firstElementChild.textContent = jogadorAtual
+                if (jogadorAtual === "X") {
+                    jogadorAtual = "O"
+                    console.log(`Vez do jogador ${jogadorAtual}`)
+                } else {
+                    jogadorAtual = "X"
+                    console.log(`Vez do jogador ${jogadorAtual}`)
+                }
             };
+            
+            if (quadrados[2].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[6].firstElementChild.textContent === "X" || quadrados[2].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[6].firstElementChild.textContent === "O") {
+                let traco = document.querySelector(".traco")
+                traco.style.display = "initial"
+                console.log("Vitória X")
+            }
         });
     };
 });
