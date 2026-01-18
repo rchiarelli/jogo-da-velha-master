156dcec9a0dda4e4ec0bd83801708355f2f74eb3
rchiarelli <renan_chiarelli@hotmail.com>
2026-01-18 16:20:00 -0300

    JavaScript

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let quadrados = document.querySelectorAll(".quadrados");
     let jogadorAtual = "X"
     
+    let traco = document.querySelector(".traco")
+    
     for (let quadrado of quadrados) {
         quadrado.addEventListener("click", function() {
             if (quadrado.firstElementChild.textContent === "") {
@@ -16,10 +18,48 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             };
             
-            if (quadrados[2].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[6].firstElementChild.textContent === "X" || quadrados[2].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[6].firstElementChild.textContent === "O") {
-                let traco = document.querySelector(".traco")
+            if (quadrados[0].firstElementChild.textContent === "X" && quadrados[1].firstElementChild.textContent === "X" && quadrados[2].firstElementChild.textContent === "X" || quadrados[0].firstElementChild.textContent === "O" && quadrados[1].firstElementChild.textContent === "O" && quadrados[2].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(223px, -380px)"
+                
+            } else if (quadrados[3].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[5].firstElementChild.textContent === "X" || quadrados[3].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[5].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(220px, -230px)"
+                console.log("Vitória X")
+                
+            } else if (quadrados[6].firstElementChild.textContent === "X" && quadrados[7].firstElementChild.textContent === "X" && quadrados[8].firstElementChild.textContent === "X" || quadrados[6].firstElementChild.textContent === "O" && quadrados[7].firstElementChild.textContent === "O" && quadrados[8].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(223px, -80px)"
+                
+            } else if (quadrados[0].firstElementChild.textContent === "X" && quadrados[3].firstElementChild.textContent === "X" && quadrados[6].firstElementChild.textContent === "X" || quadrados[0].firstElementChild.textContent === "O" && quadrados[3].firstElementChild.textContent === "O" && quadrados[6].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(63px, -237px) rotate(90deg)"
+            } else if (quadrados[1].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[7].firstElementChild.textContent === "X" || quadrados[1].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[7].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(217px, -237px) rotate(90deg)"
+                
+            } else if (quadrados[2].firstElementChild.textContent === "X" && quadrados[5].firstElementChild.textContent === "X" && quadrados[8].firstElementChild.textContent === "X" || quadrados[2].firstElementChild.textContent === "O" && quadrados[5].firstElementChild.textContent === "O" && quadrados[8].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(370px, -237px) rotate(90deg)"
+                
+            } else if (quadrados[2].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[6].firstElementChild.textContent === "X" || quadrados[2].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[6].firstElementChild.textContent === "O") {
+                
                 traco.style.display = "initial"
                 console.log("Vitória X")
+                
+            } else if (quadrados[0].firstElementChild.textContent === "X" && quadrados[4].firstElementChild.textContent === "X" && quadrados[8].firstElementChild.textContent === "X" || quadrados[0].firstElementChild.textContent === "O" && quadrados[4].firstElementChild.textContent === "O" && quadrados[8].firstElementChild.textContent === "O") {
+                
+                traco.style.display = "initial"
+                traco.style.transform = "translate(220px, -232px) rotate(45deg)"
+                
+            } else {
+                console.log(`Jogador ${jogadorAtual}, faça sua jogada`)
             }
         });
     };
